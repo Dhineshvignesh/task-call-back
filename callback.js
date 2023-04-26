9f68bd0a0b26b1cc8e9d4524220cdbe21489bc50
dhineshraja <127471949+Dhineshvignesh@users.noreply.github.com>
2023-04-26 15:17:41 +0530

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,16 @@
+const mainFunction = (callback) => {
+    setTimeout( () =>{
+        callback([2,3,4]);
+
+    }, 2000);
+
+}
+const add = (array)=>{
+let sum = 0;
+for (let i of array){
+    sum += i;
+
+}
+console.log(sum);
+};
+mainFunction(add);
